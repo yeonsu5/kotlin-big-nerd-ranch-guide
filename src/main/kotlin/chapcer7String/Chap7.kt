31c9a2fa0145a395230b9dac795545dc8dba63ed
yeonsuoh <yeonsu@hmcnetworks.co.kr>
2023-11-17 12:00:07 +0900

    7장 문자열 예제 코드 작성

---
+++ src/main/kotlin/chapcer7String/Chap7.kt
@@ -0,0 +1,46 @@
+package chapcer7String
+
+const val TAVERN_NAME = "Taernyl's Folly"
+fun main() {
+//    placeOrder("shandy,Dragon's Breath,5.91")
+    placeOrder("elixir,Shirley's Temple,4.12")
+}
+
+
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiou]")) {
+        when (it.value) {
+            "a", "A" -> "4"
+            "e", "E" -> "3"
+            "i", "I" -> "1"
+            "o", "O" -> "0"
+            "u", "U" -> "|__|"
+            else -> it.value
+        }
+    }
+
+private fun placeOrder(menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.subSequence(0 until indexOfApostrophe)
+    println("마드리갈은 $tavernMaster 에게 주문한다.")
+
+//    val data = menuData.split(',')
+//    val type = data[0]
+//    val name = data[1]
+//    val price = data[2]
+    val (type, name, price) = menuData.split(',')
+    val message = "마드리갈은 금화 $price 로 $name ($type)을 구입한다."
+    println(message)
+
+//    val phrase = "와, $name 진짜 좋구나!"
+//    println("마드리갈이 감탄한다: ${toDragonSpeak(phrase)}")
+
+    val phrase = if (name == "Dragon's Breath") {
+        "마드리갈이 감탄한다: ${toDragonSpeak("와, $name 진짜 좋구나!")}"
+    } else {
+        "마드리갈이 말한다: 감사합니다 $name."
+    }
+    println(phrase)
+
+
+}
---
+++ src/main/kotlin/chapcer7String/Unicode.kt
@@ -0,0 +1,15 @@
+package chapcer7String
+
+val capitalA: Char = 'A'
+val unicodeCapitalA: Char = '\u0041'
+
+fun main() {
+    val omSymbol = '\u0950'
+    print(omSymbol)
+
+    "Dragon's Breath".forEach {
+        println("$it\n")
+    }
+
+}
+
